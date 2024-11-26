2ac77ec79a7b2f41b5cb706dea61dcca628b6cfd
Jean Lucas Rivera <jean.rivera@usach.cl>
2024-11-26 15:54:24 -0300

    Prueba con 50 Nested Functions

---
+++ codesmells/src/main/java/com/ejemplos/codesmells/service/NestedFunctionsService2.java
@@ -0,0 +1,334 @@
+package com.ejemplos.codesmells.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NestedFunctionsService2 {
+
+    public void function26() {
+        System.out.println("Outer function 26.");
+
+        class InnerFunction26 {
+            public void innerMethod26() {
+                System.out.println("Inner function 26.");
+            }
+        }
+
+        InnerFunction26 inner = new InnerFunction26();
+        inner.innerMethod26();
+    }
+
+    public void function27() {
+        System.out.println("Outer function 27.");
+
+        class InnerFunction27 {
+            public void innerMethod27() {
+                System.out.println("Inner function 27.");
+            }
+        }
+
+        InnerFunction27 inner = new InnerFunction27();
+        inner.innerMethod27();
+    }
+
+    public void function28() {
+        System.out.println("Outer function 28.");
+
+        class InnerFunction28 {
+            public void innerMethod28() {
+                System.out.println("Inner function 28.");
+            }
+        }
+
+        InnerFunction28 inner = new InnerFunction28();
+        inner.innerMethod28();
+    }
+
+    public void function29() {
+        System.out.println("Outer function 29.");
+
+        class InnerFunction29 {
+            public void innerMethod29() {
+                System.out.println("Inner function 29.");
+            }
+        }
+
+        InnerFunction29 inner = new InnerFunction29();
+        inner.innerMethod29();
+    }
+
+    public void function30() {
+        System.out.println("Outer function 30.");
+
+        class InnerFunction30 {
+            public void innerMethod30() {
+                System.out.println("Inner function 30.");
+            }
+        }
+
+        InnerFunction30 inner = new InnerFunction30();
+        inner.innerMethod30();
+    }
+
+    public void function31() {
+        System.out.println("Outer function 31.");
+
+        class InnerFunction31 {
+            public void innerMethod31() {
+                System.out.println("Inner function 31.");
+            }
+        }
+
+        InnerFunction31 inner = new InnerFunction31();
+        inner.innerMethod31();
+    }
+
+    public void function32() {
+        System.out.println("Outer function 32.");
+
+        class InnerFunction32 {
+            public void innerMethod32() {
+                System.out.println("Inner function 32.");
+            }
+        }
+
+        InnerFunction32 inner = new InnerFunction32();
+        inner.innerMethod32();
+    }
+
+    public void function33() {
+        System.out.println("Outer function 33.");
+
+        class InnerFunction33 {
+            public void innerMethod33() {
+                System.out.println("Inner function 33.");
+            }
+        }
+
+        InnerFunction33 inner = new InnerFunction33();
+        inner.innerMethod33();
+    }
+
+    public void function34() {
+        System.out.println("Outer function 34.");
+
+        class InnerFunction34 {
+            public void innerMethod34() {
+                System.out.println("Inner function 34.");
+            }
+        }
+
+        InnerFunction34 inner = new InnerFunction34();
+        inner.innerMethod34();
+    }
+
+    public void function35() {
+        System.out.println("Outer function 35.");
+
+        class InnerFunction35 {
+            public void innerMethod35() {
+                System.out.println("Inner function 35.");
+            }
+        }
+
+        InnerFunction35 inner = new InnerFunction35();
+        inner.innerMethod35();
+    }
+
+    public void function36() {
+        System.out.println("Outer function 36.");
+
+        class InnerFunction36 {
+            public void innerMethod36() {
+                System.out.println("Inner function 36.");
+            }
+        }
+
+        InnerFunction36 inner = new InnerFunction36();
+        inner.innerMethod36();
+    }
+
+    public void function37() {
+        System.out.println("Outer function 37.");
+
+        class InnerFunction37 {
+            public void innerMethod37() {
+                System.out.println("Inner function 37.");
+            }
+        }
+
+        InnerFunction37 inner = new InnerFunction37();
+        inner.innerMethod37();
+    }
+
+    public void function38() {
+        System.out.println("Outer function 38.");
+
+        class InnerFunction38 {
+            public void innerMethod38() {
+                System.out.println("Inner function 38.");
+            }
+        }
+
+        InnerFunction38 inner = new InnerFunction38();
+        inner.innerMethod38();
+    }
+
+    public void function39() {
+        System.out.println("Outer function 39.");
+
+        class InnerFunction39 {
+            public void innerMethod39() {
+                System.out.println("Inner function 39.");
+            }
+        }
+
+        InnerFunction39 inner = new InnerFunction39();
+        inner.innerMethod39();
+    }
+
+    public void function40() {
+        System.out.println("Outer function 40.");
+
+        class InnerFunction40 {
+            public void innerMethod40() {
+                System.out.println("Inner function 40.");
+            }
+        }
+
+        InnerFunction40 inner = new InnerFunction40();
+        inner.innerMethod40();
+    }
+
+    public void function41() {
+        System.out.println("Outer function 41.");
+
+        class InnerFunction41 {
+            public void innerMethod41() {
+                System.out.println("Inner function 41.");
+            }
+        }
+
+        InnerFunction41 inner = new InnerFunction41();
+        inner.innerMethod41();
+    }
+
+    public void function42() {
+        System.out.println("Outer function 42.");
+
+        class InnerFunction42 {
+            public void innerMethod42() {
+                System.out.println("Inner function 42.");
+            }
+        }
+
+        InnerFunction42 inner = new InnerFunction42();
+        inner.innerMethod42();
+    }
+
+    public void function43() {
+        System.out.println("Outer function 43.");
+
+        class InnerFunction43 {
+            public void innerMethod43() {
+                System.out.println("Inner function 43.");
+            }
+        }
+
+        InnerFunction43 inner = new InnerFunction43();
+        inner.innerMethod43();
+    }
+
+    public void function44() {
+        System.out.println("Outer function 44.");
+
+        class InnerFunction44 {
+            public void innerMethod44() {
+                System.out.println("Inner function 44.");
+            }
+        }
+
+        InnerFunction44 inner = new InnerFunction44();
+        inner.innerMethod44();
+    }
+
+    public void function45() {
+        System.out.println("Outer function 45.");
+
+        class InnerFunction45 {
+            public void innerMethod45() {
+                System.out.println("Inner function 45.");
+            }
+        }
+
+        InnerFunction45 inner = new InnerFunction45();
+        inner.innerMethod45();
+    }
+
+    public void function46() {
+        System.out.println("Outer function 46.");
+
+        class InnerFunction46 {
+            public void innerMethod46() {
+                System.out.println("Inner function 46.");
+            }
+        }
+
+        InnerFunction46 inner = new InnerFunction46();
+        inner.innerMethod46();
+    }
+
+    public void function47() {
+        System.out.println("Outer function 47.");
+
+        class InnerFunction47 {
+            public void innerMethod47() {
+                System.out.println("Inner function 47.");
+            }
+        }
+
+        InnerFunction47 inner = new InnerFunction47();
+        inner.innerMethod47();
+    }
+
+    public void function48() {
+        System.out.println("Outer function 48.");
+
+        class InnerFunction48 {
+            public void innerMethod48() {
+                System.out.println("Inner function 48.");
+            }
+        }
+
+        InnerFunction48 inner = new InnerFunction48();
+        inner.innerMethod48();
+    }
+
+    public void function49() {
+        System.out.println("Outer function 49.");
+
+        class InnerFunction49 {
+            public void innerMethod49() {
+                System.out.println("Inner function 49.");
+            }
+        }
+
+        InnerFunction49 inner = new InnerFunction49();
+        inner.innerMethod49();
+    }
+
+    public void function50() {
+        System.out.println("Outer function 50.");
+
+        class InnerFunction50 {
+            public void innerMethod50() {
+                System.out.println("Inner function 50.");
+            }
+        }
+
+        InnerFunction50 inner = new InnerFunction50();
+        inner.innerMethod50();
+    }
+
+
+}
