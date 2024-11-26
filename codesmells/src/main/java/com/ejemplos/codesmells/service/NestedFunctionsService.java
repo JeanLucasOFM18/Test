bc489bc07d321feb6b34d82447a9703d09ce2d05
Jean Lucas Rivera <jean.rivera@usach.cl>
2024-11-26 15:37:09 -0300

    Ajuste con prueba de 5 funciones Nested

--- codesmells/src/main/java/com/ejemplos/codesmells/service/NestedFunctionsService.java
+++ codesmells/src/main/java/com/ejemplos/codesmells/service/NestedFunctionsService.java
@@ -8,66 +8,66 @@ public class NestedFunctionsService {
     public void function1() {
         System.out.println("Outer function 1.");
 
-        class InnerFunction {
-            public void innerMethod() {
+        class InnerFunction1 {
+            public void innerMethod1() {
                 System.out.println("Inner function 1.");
             }
         }
 
-        InnerFunction inner = new InnerFunction();
-        inner.innerMethod();
+        InnerFunction1 inner = new InnerFunction1();
+        inner.innerMethod1();
     }
 
     public void function2() {
         System.out.println("Outer function 2.");
 
-        class InnerFunction {
-            public void innerMethod() {
+        class InnerFunction2 {
+            public void innerMethod2() {
                 System.out.println("Inner function 2.");
             }
         }
 
-        InnerFunction inner = new InnerFunction();
-        inner.innerMethod();
+        InnerFunction2 inner = new InnerFunction2();
+        inner.innerMethod2();
     }
 
     public void function3() {
         System.out.println("Outer function 3.");
 
-        class InnerFunction {
-            public void innerMethod() {
+        class InnerFunction3 {
+            public void innerMethod3() {
                 System.out.println("Inner function 3.");
             }
         }
 
-        InnerFunction inner = new InnerFunction();
-        inner.innerMethod();
+        InnerFunction3 inner = new InnerFunction3();
+        inner.innerMethod3();
     }
 
     public void function4() {
         System.out.println("Outer function 4.");
 
-        class InnerFunction {
-            public void innerMethod() {
+        class InnerFunction4 {
+            public void innerMethod4() {
                 System.out.println("Inner function 4.");
             }
         }
 
-        InnerFunction inner = new InnerFunction();
-        inner.innerMethod();
+        InnerFunction4 inner = new InnerFunction4();
+        inner.innerMethod4();
     }
 
     public void function5() {
         System.out.println("Outer function 5.");
 
-        class InnerFunction {
-            public void innerMethod() {
+        class InnerFunction5 {
+            public void innerMethod5() {
                 System.out.println("Inner function 5.");
             }
         }
 
-        InnerFunction inner = new InnerFunction();
-        inner.innerMethod();
+        InnerFunction5 inner = new InnerFunction5();
+        inner.innerMethod5();
     }
 
 }
--- codesmells/src/main/java/com/ejemplos/codesmells/service/NestedFunctionsService2.java
+++
@@ -1,7 +0,0 @@
-package com.ejemplos.codesmells.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class NestedFunctionsService2 {
-}
