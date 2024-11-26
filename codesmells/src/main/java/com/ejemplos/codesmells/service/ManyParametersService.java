03c13c4f44e8cb9eb3b0df650cf603d49c0e8370
Jean Lucas Rivera <jean.rivera@usach.cl>
2024-11-26 16:05:25 -0300

    Prueba con 50 funciones con Many Parameters

--- codesmells/src/main/java/com/ejemplos/codesmells/service/ManyParametersService.java
+++ codesmells/src/main/java/com/ejemplos/codesmells/service/ManyParametersService.java
@@ -47,5 +47,67 @@ public class ManyParametersService {
         System.out.println("Concatenating parameters: " + a + b + c + d + e + f + g + h + i + j);
     }
 
+    public void function11(int a, int b, int c, int d, int e, int f, int g) {
+        System.out.println("Values: " + a + ", " + b + ", " + c + ", " + d + ", " + e + ", " + f + ", " + g);
+    }
+
+    public void function12(String firstName, String lastName, String middleName, int age, String city, String state, String country) {
+        System.out.println("Full Name: " + firstName + " " + middleName + " " + lastName + ", Age: " + age + ", Location: " + city + ", " + state + ", " + country);
+    }
+
+    public void function13(boolean condition1, boolean condition2, boolean condition3, boolean condition4, boolean condition5, boolean condition6) {
+        System.out.println("Conditions: " + condition1 + ", " + condition2 + ", " + condition3 + ", " + condition4 + ", " + condition5 + ", " + condition6);
+    }
+
+    public void function14(int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8) {
+        System.out.println("Numbers: " + x1 + ", " + x2 + ", " + x3 + ", " + x4 + ", " + x5 + ", " + x6 + ", " + x7 + ", " + x8);
+    }
+
+    public void function15(String p1, String p2, String p3, String p4, String p5, String p6, String p7, String p8, String p9) {
+        System.out.println("Parameters: " + p1 + ", " + p2 + ", " + p3 + ", " + p4 + ", " + p5 + ", " + p6 + ", " + p7 + ", " + p8 + ", " + p9);
+    }
+
+    public void function16(double value1, double value2, double value3, double value4, double value5, double value6, double value7) {
+        double product = value1 * value2 * value3 * value4 * value5 * value6 * value7;
+        System.out.println("Product of values: " + product);
+    }
+
+    public void function17(int param1, int param2, int param3, int param4, int param5, int param6, int param7, int param8, int param9) {
+        System.out.println("Sum of params: " + (param1 + param2 + param3 + param4 + param5 + param6 + param7 + param8 + param9));
+    }
+
+    public void function18(String key1, String key2, String key3, String key4, String key5, String key6, String key7, String key8) {
+        System.out.println("Keys: " + key1 + ", " + key2 + ", " + key3 + ", " + key4 + ", " + key5 + ", " + key6 + ", " + key7 + ", " + key8);
+    }
+
+    public void function19(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10) {
+        System.out.println("Max count parameters: " + a1 + ", " + a2 + ", " + a3 + ", " + a4 + ", " + a5 + ", " + a6 + ", " + a7 + ", " + a8 + ", " + a9 + ", " + a10);
+    }
+
+    public void function20(String arg1, String arg2, String arg3, String arg4, String arg5, String arg6, String arg7, String arg8, String arg9, String arg10) {
+        System.out.println("Arguments: " + arg1 + ", " + arg2 + ", " + arg3 + ", " + arg4 + ", " + arg5 + ", " + arg6 + ", " + arg7 + ", " + arg8 + ", " + arg9 + ", " + arg10);
+    }
+
+    public void function21(int x1, int x2, int x3, int x4, int x5, int x6, int x7) {
+        int sum = x1 + x2 + x3 + x4 + x5 + x6 + x7;
+        System.out.println("Sum: " + sum);
+    }
+
+    public void function22(String str1, String str2, String str3, String str4, String str5, String str6) {
+        System.out.println("Concatenated: " + str1 + str2 + str3 + str4 + str5 + str6);
+    }
+
+    public void function23(double val1, double val2, double val3, double val4, double val5, double val6, double val7, double val8) {
+        double avg = (val1 + val2 + val3 + val4 + val5 + val6 + val7 + val8) / 8;
+        System.out.println("Average: " + avg);
+    }
+
+    public void function24(int a, int b, int c, int d, int e, int f, int g, int h, int i) {
+        System.out.println("Multiplication: " + (a * b * c * d * e * f * g * h * i));
+    }
+
+    public void function25(boolean bool1, boolean bool2, boolean bool3, boolean bool4, boolean bool5, boolean bool6, boolean bool7) {
+        System.out.println("Booleans: " + bool1 + ", " + bool2 + ", " + bool3 + ", " + bool4 + ", " + bool5 + ", " + bool6 + ", " + bool7);
+    }
 
 }
