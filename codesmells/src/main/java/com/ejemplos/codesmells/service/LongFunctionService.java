148b059516a44432cad5f6cba224b7dc2d209a3c
Jean Lucas Rivera <jean.rivera@usach.cl>
2024-12-20 11:35:26 -0300

    Prueba de 50 funciones sin problemas

--- codesmells/src/main/java/com/ejemplos/codesmells/service/LongFunctionService.java
+++ codesmells/src/main/java/com/ejemplos/codesmells/service/LongFunctionService.java
@@ -1,125 +1,113 @@
 package com.ejemplos.codesmells.service;
 
-import org.springframework.stereotype.Service;
+public class LongFunctionService {
 
-import java.util.Arrays;
-
-@Service
-public class LongFunctionService2 {
-
-    public void greet() {
-        System.out.println("Hello, World!");
+    public void printDivider() {
+        System.out.println("-------------");
     }
 
-    public int add(int a, int b) {
-        return a + b;
+    public boolean startsWithA(String str) {
+        return str != null && str.startsWith("A");
     }
 
-    public boolean isEven(int number) {
-        int x = 2;
-        int y = 0;
-        return number % x == y;
+    public int absoluteValue(int number) {
+        int x = 0;
+        return (number < x) ? -number : number;
     }
 
-    public void printMessage(String message) {
-        System.out.println(message);
+    public void greetMorning() {
+        System.out.println("Good Morning!");
     }
 
-    public int multiply(int x, int y) {
-        return x * y;
+    public boolean isUpperCase(String str) {
+        return str.equals(str.toUpperCase());
+    }
+    public void printGoodbye() {
+        System.out.println("Goodbye!");
     }
 
-    public void displayDate() {
-        System.out.println(java.time.LocalDate.now());
+    public int addThreeNumbers(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public boolean isEvenLength(String str) {
+        int a = 2;
+        int b = 0;
+        return str.length() % a == b;
+    }
+
+    public void printUpperCase(String str) {
+        System.out.println(str.toUpperCase());
     }
 
     public int findMax(int a, int b) {
         return (a > b) ? a : b;
     }
 
-    public double calculateCircleArea(double radius) {
-        return Math.PI * radius * radius;
+    public void printWelcome(String name) {
+        System.out.println("Welcome, " + name + "!");
     }
 
-    public void printNumbers(int limit) {
-        for (int i = 1; i <= limit; i++) {
-            System.out.println(i);
-        }
+    public boolean endsWithExclamation(String str) {
+        return str != null && str.endsWith("!");
     }
 
-    public boolean isPositive(int number) {
-        return number > 0;
+    public int doubleValue(int number) {
+        int c = 2;
+        return number * c;
     }
 
-    public int subtract(int a, int b) {
+    public void printTodayDate() {
+        System.out.println(java.time.LocalDate.now());
+    }
+
+    public boolean isPalindrome(String str) {
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
+    }
+
+    public void printSuccess() {
+        System.out.println("Operation successful!");
+    }
+
+    public int subtractNumbers(int a, int b) {
         return a - b;
     }
 
-    public boolean isOdd(int number) {
-        int w = 2;
-        int a = 0;
-        return number % w != a;
+    public boolean isAlphabetic(String str) {
+        return str.matches("[a-zA-Z]+");
     }
 
-    public void printArray(int[] arr) {
-        for (int num : arr) {
-            System.out.println(num);
-        }
+    public void printLowerCase(String str) {
+        System.out.println(str.toLowerCase());
     }
 
-    public double divide(int a, int b) {
-        int x = 0;
-        if (b != x) {
-            return (double) a / b;
-        }
-        return x;
-    }
-
-    public void printGreeting(String name) {
-        System.out.println("Hello, " + name + "!");
-    }
-
-    public boolean containsChar(String str, char c) {
-        return str.indexOf(c) != -1;
-    }
-
-    public int square(int number) {
+    public int squareNumber(int number) {
         return number * number;
     }
 
-    public void displayTime() {
-        System.out.println(java.time.LocalTime.now());
+    public void printErrorMessage() {
+        System.out.println("An error has occurred!");
     }
 
-    public double calculateAverage(int a, int b) {
-        double x = 2.0;
-        return (a + b) / x;
+    public boolean containsSpace(String str) {
+        return str.contains(" ");
     }
 
-    public boolean isEmpty(String str) {
-        return str == null || str.isEmpty();
-    }
-
-    public void printHello() {
-        System.out.println("Hello!");
-    }
-
-    public int multiplyByTwo(int number) {
-        return number * 2;
-    }
-
-    public boolean isNegative(int number) {
-        return number < 0;
-    }
-
-    public void printFirstChar(String str) {
-        int z = 0;
-        if (str != null && !str.isEmpty()) {
-            System.out.println(str.charAt(z));
+    public int sumArray(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
         }
+        return sum;
     }
 
-    public int findMin(int a, int b) {
-        return (a < b) ? a : b;
+    public void printThankYou() {
+        System.out.println("Thank you!");
     }
+
+    public boolean isEmptyString(String str) {
+        return str.isEmpty();
+    }
+
 }
