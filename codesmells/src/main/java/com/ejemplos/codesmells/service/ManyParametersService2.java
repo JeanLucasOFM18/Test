dd25c9db3aa1a63adccc129d29674cdbe94d3ac7
Jean Lucas Rivera <jean.rivera@usach.cl>
2024-12-20 12:49:42 -0300

    Prueba de 25 funciones con muchos parámetros y 25 sanas

--- codesmells/src/main/java/com/ejemplos/codesmells/service/ManyParametersService2.java
+++ codesmells/src/main/java/com/ejemplos/codesmells/service/ManyParametersService2.java
@@ -5,117 +5,112 @@ import org.springframework.stereotype.Service;
 @Service
 public class ManyParametersService2 {
 
-    public void function26(String name1, String name2, String name3, String name4, String name5, String name6) {
-        System.out.println("Names: " + name1 + ", " + name2 + ", " + name3 + ", " + name4 + ", " + name5 + ", " + name6);
+    public void printDivider() {
+        System.out.println("-------------");
     }
 
-    public void function27(float f1, float f2, float f3, float f4, float f5, float f6, float f7) {
-        float result = f1 - f2 + f3 * f4 / f5 + f6 - f7;
-        System.out.println("Result: " + result);
+    public boolean startsWithA(String str) {
+        return str != null && str.startsWith("A");
     }
 
-    public void function28(int num1, int num2, int num3, int num4, int num5, int num6, int num7, int num8) {
-        int max = Math.max(Math.max(Math.max(num1, num2), Math.max(num3, num4)), Math.max(Math.max(num5, num6), Math.max(num7, num8)));
-        System.out.println("Max number: " + max);
+    public int absoluteValue(int number) {
+        int x = 0;
+        return (number < x) ? -number : number;
     }
 
-    public void function29(String arg1, String arg2, String arg3, String arg4, String arg5, String arg6, String arg7, String arg8) {
-        System.out.println("Arguments: " + arg1 + " | " + arg2 + " | " + arg3 + " | " + arg4 + " | " + arg5 + " | " + arg6 + " | " + arg7 + " | " + arg8);
+    public void greetMorning() {
+        System.out.println("Good Morning!");
     }
 
-    public void function30(int x, int y, int z, int w, int u, int v, int p, int q, int r, int s) {
-        System.out.println("Values: " + x + ", " + y + ", " + z + ", " + w + ", " + u + ", " + v + ", " + p + ", " + q + ", " + r + ", " + s);
+    public boolean isUpperCase(String str) {
+        return str.equals(str.toUpperCase());
+    }
+    public void printGoodbye() {
+        System.out.println("Goodbye!");
     }
 
-    public void function31(int a, int b, int c, int d, int e, int f, int g, int h, int i) {
-        int product = a * b * c * d * e * f * g * h * i;
-        System.out.println("Product: " + product);
+    public int addThreeNumbers(int a, int b, int c) {
+        return a + b + c;
     }
 
-    public void function32(String str1, String str2, String str3, String str4, String str5) {
-        System.out.println("Concatenated strings: " + str1 + str2 + str3 + str4 + str5);
+    public boolean isEvenLength(String str) {
+        int a = 2;
+        int b = 0;
+        return str.length() % a == b;
     }
 
-    public void function33(double val1, double val2, double val3, double val4, double val5, double val6) {
-        double sum = val1 + val2 + val3 + val4 + val5 + val6;
-        System.out.println("Sum: " + sum);
+    public void printUpperCase(String str) {
+        System.out.println(str.toUpperCase());
     }
 
-    public void function34(int x1, int x2, int x3, int x4, int x5, int x6) {
-        int difference = x1 - x2 - x3 - x4 - x5 - x6;
-        System.out.println("Difference: " + difference);
+    public int findMax(int a, int b) {
+        return (a > b) ? a : b;
     }
 
-    public void function35(float f1, float f2, float f3, float f4, float f5) {
-        float division = f1 / f2 / f3 / f4 / f5;
-        System.out.println("Division result: " + division);
+    public void printWelcome(String name) {
+        System.out.println("Welcome, " + name + "!");
     }
 
-    public void function36(boolean bool1, boolean bool2, boolean bool3, boolean bool4, boolean bool5) {
-        System.out.println("All true: " + (bool1 && bool2 && bool3 && bool4 && bool5));
+    public boolean endsWithExclamation(String str) {
+        return str != null && str.endsWith("!");
     }
 
-    public void function37(int num1, int num2, int num3, int num4, int num5) {
-        System.out.println("Average: " + (num1 + num2 + num3 + num4 + num5) / 5);
+    public int doubleValue(int number) {
+        int c = 2;
+        return number * c;
     }
 
-    public void function38(String name1, String name2, String name3, String name4, String name5) {
-        System.out.println("Names list: " + name1 + ", " + name2 + ", " + name3 + ", " + name4 + ", " + name5);
+    public void printTodayDate() {
+        System.out.println(java.time.LocalDate.now());
     }
 
-    public void function39(double val1, double val2, double val3, double val4, double val5, double val6, double val7) {
-        double result = (val1 + val2 + val3) / (val4 + val5 + val6 + val7);
-        System.out.println("Result: " + result);
+    public boolean isPalindrome(String str) {
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
     }
 
-    public void function40(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j) {
-        int sum = a + b + c + d + e + f + g + h + i + j;
-        System.out.println("Sum: " + sum);
+    public void printSuccess() {
+        System.out.println("Operation successful!");
     }
 
-    public void function41(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j) {
-        int product = a * b * c * d * e * f * g * h * i * j;
-        System.out.println("Product: " + product);
+    public int subtractNumbers(int a, int b) {
+        return a - b;
     }
 
-    public void function42(double v1, double v2, double v3, double v4, double v5, double v6, double v7, double v8, double v9, double v10) {
-        double average = (v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10) / 10;
-        System.out.println("Average: " + average);
+    public boolean isAlphabetic(String str) {
+        return str.matches("[a-zA-Z]+");
     }
 
-    public void function43(String s1, String s2, String s3, String s4, String s5, String s6, String s7, String s8, String s9, String s10) {
-        System.out.println("Combined String: " + s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9 + s10);
+    public void printLowerCase(String str) {
+        System.out.println(str.toLowerCase());
     }
 
-    public void function44(int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, int x9, int x10) {
-        System.out.println("Product of numbers: " + (x1 * x2 * x3 * x4 * x5 * x6 * x7 * x8 * x9 * x10));
+    public int squareNumber(int number) {
+        return number * number;
     }
 
-    public void function45(float f1, float f2, float f3, float f4, float f5, float f6, float f7, float f8, float f9, float f10) {
-        float total = f1 + f2 + f3 + f4 + f5 + f6 + f7 + f8 + f9 + f10;
-        System.out.println("Total sum: " + total);
+    public void printErrorMessage() {
+        System.out.println("An error has occurred!");
     }
 
-    public void function46(boolean b1, boolean b2, boolean b3, boolean b4, boolean b5, boolean b6, boolean b7, boolean b8, boolean b9, boolean b10) {
-        System.out.println("All true: " + (b1 && b2 && b3 && b4 && b5 && b6 && b7 && b8 && b9 && b10));
+    public boolean containsSpace(String str) {
+        return str.contains(" ");
     }
 
-    public void function47(int n1, int n2, int n3, int n4, int n5, int n6, int n7, int n8, int n9, int n10) {
-        System.out.println("Sum: " + (n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10));
+    public int sumArray(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
     }
 
-    public void function48(double d1, double d2, double d3, double d4, double d5, double d6, double d7, double d8, double d9, double d10) {
-        double result = d1 - d2 - d3 - d4 - d5 - d6 - d7 - d8 - d9 - d10;
-        System.out.println("Result: " + result);
+    public void printThankYou() {
+        System.out.println("Thank you!");
     }
 
-    public void function49(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8, int p9, int p10) {
-        int diff = p1 - p2 - p3 - p4 - p5 - p6 - p7 - p8 - p9 - p10;
-        System.out.println("Difference: " + diff);
-    }
-
-    public void function50(String name1, String name2, String name3, String name4, String name5, String name6, String name7, String name8, String name9, String name10) {
-        System.out.println("Names list: " + name1 + ", " + name2 + ", " + name3 + ", " + name4 + ", " + name5 + ", " + name6 + ", " + name7 + ", " + name8 + ", " + name9 + ", " + name10);
+    public boolean isEmptyString(String str) {
+        return str.isEmpty();
     }
 
 }
